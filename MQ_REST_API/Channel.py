7079dcfd501611f24eab004d879e36ba5b321959
Hgiddins <hugogiddins1@gmail.com>
2023-09-12 23:46:10 +0100

    channels added fields and removed dependency graph

--- MQ_REST_API/Channel.py
+++ MQ_REST_API/Channel.py
@@ -1,39 +1,22 @@
 class Channel:
-    def __init__(self, channel_name=None, channel_type=None):
-        self.channel_name = channel_name
-        self.channel_type = channel_type
+    def __init__(self, channelName=None, channelType=None, description=None,
+                 maxMessageLength=None, heartbeatInterval=None, transportType=None):
+        self.channelName = channelName
+        self.channelType = channelType
+        self.description = description
+        self.maxMessageLength = maxMessageLength
+        self.heartbeatInterval = heartbeatInterval
+        self.transportType = transportType
 
     def __str__(self):
         return str(self.__dict__)
 
     def to_dict(self):
         return {
-            "channel_name": self.channel_name,
-            "channel_type": self.channel_type,
+            "channel_name": self.channelName,
+            "channel_type": self.channelType,
+            "description": self.description,
+            "max_message_length": self.maxMessageLength,
+            "heartbeat_interval": self.heartbeatInterval,
+            "transport_type": self.transportType,
         }
-
-class SenderChannel(Channel):
-    def __init__(self, channel_name=None, channel_type=None, transmission_queue_name=None, connection_name=None):
-        super().__init__(channel_name, channel_type)
-        self.transmission_queue_name = transmission_queue_name
-        self.connection_name = connection_name
-
-    def to_dict(self):
-        return {
-            "channel_name": self.channel_name,
-            "channel_type": self.channel_type,
-            "transmission_queue_name": self.transmission_queue_name,
-            "connection_name": self.connection_name,
-        }
-
-class ReceiverChannel(Channel):
-    def __init__(self, channel_name=None, channel_type=None):
-        super().__init__(channel_name, channel_type)
-
-    # Inherits to_dict from Channel
-
-class ApplicationChannel(Channel):
-    def __init__(self, channel_name=None, channel_type=None):
-        super().__init__(channel_name, channel_type)
-
-    # Inherits to_dict from Channel
